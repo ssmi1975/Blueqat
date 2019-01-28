da1e8f2645efc51ba3b285490678fcbf3aa62950
gyu-don <takumi.kt+git@gmail.com>
2019-01-28 23:37:25 +0900

    wip

--- blueqat/qasmparser.py
+++ blueqat/qasmparser.py
@@ -1,15 +1,20 @@
+from abc import abstractmethod
+from enum import Enum
 import re
+from typing import Any, Dict, List, Set
 from itertools import takewhile
 
-_regex_tokens = re.compile(r'OPENQASM 2.0|"[^"]+"|[\w_]+|//|[0-9.]+|[=;*+-/(/)\[\]]')
-
-
+_regex_tokens = re.compile(r'OPENQASM 2.0|"[^"]+"|[a-zA-Z_][a-zA-Z_0-9]+|//|[0-9.]+|\S')
 def split_tokens(qasmstr):
     for i, line in enumerate(qasmstr.split('\n')):
         toks = _regex_tokens.findall(line)
         yield from takewhile(
                 lambda x: not x[1].startswith('//'),
                 ((i, tok) for tok in toks))
+
+
+def _err_with_lineno(lineno, msg):
+    raise ValueError(f"Line {lineno}: {msg}")
 
 
 class TokenGetter:
@@ -39,7 +44,7 @@ class TokenGetter:
         if action is None:
             return None
         elif isinstance(action, str):
-            raise ValueError(f'Line {self.lineno}: {action}')
+            _err_with_lineno(self.lineno, action)
 
 
     def get_if(self, cond, or_else=None):
@@ -59,13 +64,22 @@ class TokenGetter:
         raise ValueError('Unknown conditions')
 
 
+    def assert_semicolon(self, msg='";" is expected.'):
+        self.get_if(';', msg)
+
+
 def parse_qasm(qasmstr):
     tokens = TokenGetter(split_tokens(qasmstr))
     errmsg = 'Program shall be start with "OPENQASM 2.0;".'
     tokens.get_if('OPENQASM 2.0', errmsg)
-    tokens.get_if(';', errmsg)
-    args = _parse_statements(tokens)
-    return QasmProgram(*args)
+    tokens.assert_semicolon()
+    stmts = []
+    gates = {}
+    qregs = {}
+    cregs = {}
+    included = set()
+    _parse_statements(tokens, stmts, gates, qregs, cregs, included)
+    return QasmProgram(stmts, gates, qregs, cregs, included)
 
 
 def parse_qasmf(qasmfile, *args, **kwargs):
@@ -80,48 +94,133 @@ class QasmNode:
 
 
 class QasmProgram(QasmNode):
-    def __init__(self, statements, gates, qregs, cregs):
+    def __init__(self,
+                 statements: List[Any],
+                 qregs: Dict[str, int],
+                 cregs: Dict[str, int],
+                 gates: Dict[str, Any],
+                 included: Set[str]):
         self.statements = statements
-        self.gates = gates
         self.qregs = qregs
         self.cregs = cregs
+        self.gates = gates
+        self.included = included
 
 
     def __repr__(self):
         return f'QasmProgram({repr(self.statements)}, ' + \
-               f'{repr(self.gates)}, ' + \
-               f'{repr(self.qregs)}, {repr(self.cregs)})'
+               f'{repr(self.qregs)}, {repr(self.cregs)}, ' + \
+               f'{repr(self.gates)}, {repr(self.included)})'
 
 
-_re_symbol = re.compile(r'^[a-zA-Z_][a-zA-Z0-9_]*$')
+class QasmGateDef(QasmNode):
+    pass
 
-def _is_symbol(s):
-    return _re_symbol.match(s)
 
-_re_uint = re.compile(r'^[1-9][0-9]*$')
+QasmGateType = Enum('QasmGateType', 'Gate Opaque Builtin')
 
-def _is_uint(s):
-    return _re_uint.match(s)
 
-def _parse_statements(tokens):
-    stmts = []
-    qregs = {}
-    cregs = {}
-    gates = {}
+class QasmAbstractGate:
+    @classmethod
+    @abstractmethod
+    def gatetype(cls) -> QasmGateType:
+        pass
+
+
+class QasmGate(QasmAbstractGate):
+    @classmethod
+    def gatetype(cls):
+        return QasmGateType.Gate
+
+    def __init__(self, gatedef: QasmGateDef):
+        self.gatedef = gatedef
+        self.name = '' # TODO: impl.
+
+
+    def __repr__(self):
+        return f'QasmGate({repr(self.gatedef)})'
+
+
+class QasmOpaque(QasmAbstractGate):
+    @classmethod
+    def gatetype(cls):
+        return QasmGateType.Opaque
+
+    def __init__(self, name: str):
+        self.name = name
+
+
+    def __repr__(self):
+        return f"QasmOpaque('{self.name}')"
+
+
+class QasmBuiltinGate(QasmAbstractGate):
+    @classmethod
+    def gatetype(cls):
+        return QasmGateType.Builtin
+
+
+    def __init__(self, name: str):
+        self.name = name
+
+
+    def __repr__(self):
+        return f"QasmBuiltinGate('{self.name}')"
+
+
+def _get_matcher(regex):
+    _re = re.compile(regex)
+    def matcher(s):
+        return _re.match(s)
+    return matcher
+
+
+_is_symbol = _get_matcher(r'^[a-zA-Z_][a-zA-Z0-9_]*$')
+_is_quoted_str = _get_matcher(r'^"[^"]*"$')
+_is_uint = _get_matcher(r'^[1-9][0-9]*$')
+
+
+def _parse_statements(tokens,
+                      stmts: List[Any],
+                      qregs: Dict[str, int],
+                      cregs: Dict[str, int],
+                      gates: Dict[str, Any],
+                      included: Set[str]):
     lineno, tok = tokens.get()
     while tok:
         if tok == 'qreg':
             sym, num = _parse_reg(tokens)
             if sym in qregs or sym in cregs:
-                raise ValueError(f'Register "{sym}" is already defined.')
+                _err_with_lineno(lineno, f'Register "{sym}" is already defined.')
             qregs[sym] = num
         elif tok == 'creg':
             sym, num = _parse_reg(tokens)
             if sym in qregs or sym in cregs:
-                raise ValueError(f'Register "{sym}" is already defined.')
+                _err_with_lineno(lineno, f'Register "{sym}" is already defined.')
             cregs[sym] = num
+        elif tok == 'include':
+            incfile = _parse_include_stmt(tokens)
+            if incfile in included:
+                _err_with_lineno(lineno, f'File "{incfile}" is already included.')
+            included.add(incfile)
+            if incfile == "qelib1.inc":
+                load_qelib1(gates)
+            else:
+                try:
+                    with open(incfile) as f:
+                        _parse_statements(f.read(), stmts, qregs, cregs, gates, included)
+                except FileNotFoundError:
+                    _err_with_lineno(lineno, f'Included file "{incfile}" is not exists.')
+                except IsADirectoryError:
+                    _err_with_lineno(lineno, f'Included file "{incfile}" is a directory.')
+                except PermissionError:
+                    _err_with_lineno(lineno, f'Cannot access to "{incfile}". Permission denied.')
+                except OSError as e:
+                    _err_with_lineno(lineno, f'During reading file {incfile}, Error occured. {e}')
+        #elif tok == 
+        else:
+            print(f"?{lineno}: {tok}")
         lineno, tok = tokens.get()
-    return [], {}, qregs, cregs
 
 
 def _parse_reg(tokens):
@@ -129,8 +228,23 @@ def _parse_reg(tokens):
     tokens.get_if('[', f'Unexpected token after "qreg {sym}".')
     num = tokens.get_if(_is_uint, f'After "qreg {sym}[", unsigned integer is expected.')
     tokens.get_if(']', 'Unclosed bracket "[".')
-    tokens.get_if(';', '";" not found.')
+    tokens.assert_semicolon()
     return sym[1], int(num[1])
+
+
+def _parse_include_stmt(tokens):
+    incfile = tokens.get_if(_is_quoted_str, 'After "include", file path is expected.')
+    tokens.assert_semicolon()
+    return incfile[1][1:-1]
+
+
+def load_qelib1(gates):
+    from blueqat.circuit import GATE_SET
+    for gate in GATE_SET:
+        gates[gate] = QasmBuiltinGate(GATE_SET[gate].lowername)
+    # TODO: These gates are not defined in qelib1, but defined in language specifications.
+    gates['U'] = QasmBuiltinGate('u3')
+    gates['CX'] = QasmBuiltinGate('cx')
 
 
 if __name__ == '__main__':
@@ -142,7 +256,7 @@ OPENQASM 2.0;
 include "qelib1.inc";
 qreg q[4];
 creg c[4];
-x q[0]; 
+x q[0];
 x q[2];
 barrier q;
 h q[0];
